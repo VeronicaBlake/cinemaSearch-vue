4539e37fa9116901bd7ab104c3af5ace6bd129a5
veronicaBlake <vrex149@gmail.com>
2021-10-18 11:23:45 -0600

    init

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+    <div>
+      <h1>
+        <i class="fas fa-video text-primary"></i>
+        Cinema Search
+      </h1>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  setup() {
+  }
+}
+</script>
+
+<style scoped>
+.dropdown-menu {
+  user-select: none;
+  display: block;
+  transform: scale(0);
+  transition: all 0.15s linear;
+}
+.dropdown-menu.show {
+  transform: scale(1);
+}
+.hoverable {
+  cursor: pointer;
+}
+a:hover {
+  text-decoration: none;
+}
+.nav-link{
+  text-transform: uppercase;
+}
+.navbar-nav .router-link-exact-active{
+  border-bottom: 2px solid var(--bs-success);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="containter">
+    <div class="row">
+      <div class="col-md-3">
+        <div class="row">
+          <div class="input-group">
+            <div class="form-outline">
+              <input type="search" id="form1" class="form-control" />
+              <label class="form-label" for="form1">Search</label>
+            </div>
+            <button type="button" class="btn btn-primary">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        Here's where the active movie stuff goes
+      </div>
+      <div class="col-md-3">
+        <h1>Other Movies You Might Like: </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// set up the axios endpoint
+// set up the appstate?
+// search two criteria
+// have a space where the search list is
+// space for active movie
+// space for movies in related genres
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped lang="scss">
+.home{
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+  .home-card{
+    width: 50vw;
+    > img{
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
